146c5f4bdfd110b0463ecadbf48f2eb2d82f9de8
Deborah4977 <87955335+Deborah4977@users.noreply.github.com>
2022-04-25 21:02:46 -0300

    Add files via upload

---
+++ Pomodoro/Pomodoro.js
@@ -0,0 +1,3 @@
+const Pomodoro = <h1>Hello, world</h1>;
+
+export default Pomodoro;
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Pomodoro from "../Pomodoro/Pomodoro.js";
+import Pomodoro from "./my-app/Pomodoro/Pomodoro";
+
+function App() {
+  return (
+    <div>
+      <Pomodoro />
+    </div>
+  );
+}
+
+export default App;
